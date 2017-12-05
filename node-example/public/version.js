cf59e662b1b05e1050545b38314efdbe5ebbc91c
James Abele <James.Abele@fda.hhs.gov>
2017-12-04 19:13:47 -0600

    working on a cleaner install/start

--- node-example/public/version.js
+++ node-example/public/version.js
@@ -2,11 +2,11 @@
 
 var version = {
 		"name":      "react-with-node-example",
-		"buildTime": "12:40:42.753Z",
-		"buildDate": "2017-05-09",
+		"buildTime": "03:19:59.734Z",
+		"buildDate": "2017-11-30",
 		"version":   "0.1.0",
-		"NodeJS":    "v7.9.0",
+		"NodeJS":    "v8.8.1",
 		"PkgReact":     "^15.4.2",
 		"PkgWebPack":   "^2.5.0",
-    "WebPack":   "2.5.1"
+    "WebPack":   "2.7.0"
 	}
